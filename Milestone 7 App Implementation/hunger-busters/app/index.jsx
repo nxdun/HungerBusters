bc075bc20eefe154dca6d4c5c8700e3aa6a17101
Vish0111 <it21315282@my.sliit.lk>
2024-09-11 01:22:37 +0530

    Onbording Screen Done

--- Milestone 7 App Implementation/hunger-busters/app/index.jsx
+++ Milestone 7 App Implementation/hunger-busters/app/index.jsx
@@ -1,15 +1,49 @@
-import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Image, ScrollView, Text, View } from "react-native";
+import { Redirect, router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl font-pblack">HungerBusters</Text>
-    <StatusBar style="auto" />
-    <Link href="/home" style={{
-        color: "blue",
-      }}>Go to Home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height:'100%'}}>
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
+          <Image 
+            source={images.logo}
+            className="w-[200px] h-[160px]"
+            resizeMode="contain"
+          />
+          <Image 
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl font-bold text-center text-white">
+              Welcome to<Text className="text-secondary-200"> Hunger Busters</Text>
+            </Text>
+            <Image 
+            source={images.path}
+            className="w-[130px] h-[15px] absolute -bottom-2 -right-1"
+            resizeMode="contain"
+          />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center ">
+            Food for Everyone Zero Hunger
+          </Text>
+
+          <CustomButton title="Get Started"
+          handlePress={()=> router.push('/sign-in')}
+          containerStyles="w-full mt-7" 
+          />
+
+          <StatusBar backgroundColor="#161622" style="light" />
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
